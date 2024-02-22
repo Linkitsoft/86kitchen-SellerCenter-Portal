2fab43f438e0e4e5dc72b3e2ba53a9b062698a84
alishanmomin <mominalishan@gmail.com>
2024-02-22 16:13:25 +0500

    fix

--- src/route/adminPrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-function AdminPrivate({ token }) {
-    return token ? <Outlet /> : <Navigate to="/login" />;
-}
-  
-
-export default AdminPrivate;
--- src/util/webUrls.js
+++
@@ -1,6 +0,0 @@
-export const webUrls = {
-    marketPlace: "http://3.139.69.64/",
-    affiliate: "http://18.191.212.109/",
-    partnerCenter: "https://86kitchensellercenter.netlify.app/",
-    superAdmin: "https://86kitchenadmin.netlify.app/dashboard",
-}
